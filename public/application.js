8bb70ad25cdb4cf2d5e44a64a38c43bad154c545
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2015-11-20 22:55:25 +0800

    Add animate on drag

--- public/application.js
+++ public/application.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
   $('#game_menu').draggable({
     scroll: false,
-    appendTo: "body"
+    start: function(event, ui) {
+      $(this).animate({
+        padding: '+=25px'
+      });
+    },
+    stop: function(event, ui) {
+      $(this).animate({
+        padding: '-=25px'
+      }, 250);
+    }
   });
+  // $('#game_menu').mouseenter(function() {
+  //   $(this).animate({
+  //     height: '+=20px',
+  //     width: '+=20px'
+  //   });
+  // });
+  // $('#game_menu').mouseleave(function() {
+  //   $(this).animate({
+  //     height: '-=20px',
+  //     width: '-=20px'
+  //   });
+  // });
 });
